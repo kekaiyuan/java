4afa9b97aee22f4af7ad18f34c0797e1f598759c
kekaiyuan <1054623598@qq.com>
2021-08-22 15:43:09 +0800

    8.22

---
+++ framework/springmvc/springmvc_json/src/main/java/com/kky/controller/MyController.java
@@ -0,0 +1,15 @@
+package com.kky.controller;
+
+import org.springframework.stereotype.Controller;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Controller
+public class MyController {
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
